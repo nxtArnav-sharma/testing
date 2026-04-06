2da706bd61838c02f3fb8768bb56e310954ea67c
Arnav Sharma <166384728+nxtArnav-sharma@users.noreply.github.com>
2026-04-07 02:12:19 +0900

    Remove duplicate console.log statements

--- console.js
+++ console.js
@@ -6,3 +6,11 @@ console.log("Hello World")
 console.log("Hello World")
 
 console.log("Hello World")
+// this is a test repo
+console.log("Hello World")
+
+console.log("Hello World")
+// this is a test repo
+console.log("Hello World")
+
+console.log("Hello World")
